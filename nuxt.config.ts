da7f9be3a8d315621b96636ea7f5f92232d6e2f8
이희재 <hj.lee2@stickint.kr>
2023-11-30 15:43:11 +0900

    Modify meta 이미지 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,16 @@ export default defineNuxtConfig({
       title: 'ToDo List',
       meta: [
         { name: 'description', content: '오늘의 할 일은 무엇일까요?' },
-        { name: 'image', content: 'https://heejae0811.github.io/nuxt3-todo/assets/images/meta.jpg'}
+        { name: 'image', content: 'https://heejae0811.github.io/nuxt3-todo/meta.jpg'}
+      ],
+      link: [
+        {
+          rel: 'icon', type: 'image/x-icon', href: 'https://heejae0811.github.io/nuxt3-todo/favicon.ico'
+        }
       ]
     },
     baseURL: '/nuxt3-todo/',
-    buildAssetsDir: 'assets'
+    buildAssetsDir: '/assets/'
   },
   devtools: { enabled: true },
   modules: [
