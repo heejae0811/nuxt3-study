8caa00ccaa9678a7860df5f538394eaa41aba2a2
이희재 <hj.lee2@stickint.kr>
2023-12-26 13:53:39 +0900

    Modify 상세페이지 dayjs

--- pages/[id].vue
+++ pages/[id].vue
@@ -9,7 +9,7 @@ const { data } = await useFetch(`http://localhost:3001/todo/${route.params.id}`)
   <section>
     <h1 class="mt-6 mb-12 md:mt-10 text-5xl font-bold text-center">To Do Detail</h1>
 
-    <div class="p-4 md:p-6 md:pb-10 border rounded">
+    <div class="p-4 md:p-6 md:pb-8 border rounded">
       <div class="flex justify-between mb-8">
         <p class="text-xl font-bold">No.{{ data.id }}</p>
         <p class="text-xl font-bold">{{ dayjs(data.created_at).format('YYYY.MM.DD') }}</p>
